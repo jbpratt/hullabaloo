87de27eecca5a6ab29062ddd2ced384f74daa747
jbpratt78 <jbpratt78@gmail.com>
2019-07-29 20:57:06 -0500

    basic arrow keys

--- main.c
+++ main.c
@@ -1,68 +1,102 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
-const int SCREEN_HEIGHT = 560;
-const int SCREEN_WIDTH = 960;
+const int SCREEN_HEIGHT = 1000;
+const int SCREEN_WIDTH = 1000;
+SDL_Window *gWindow;
+SDL_Surface *gScreen;
+SDL_Renderer *gRenderer;
 
-int main(int argc, char *args[]) {
+int init() {
   if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
     printf("SDL_Init Error: %s\n", SDL_GetError());
-    exit(1);
+    return 1;
   }
 
-  SDL_Window *window = SDL_CreateWindow("Majora", SDL_WINDOWPOS_UNDEFINED,
-                                        SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                                        SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  gWindow = SDL_CreateWindow("Majora", SDL_WINDOWPOS_UNDEFINED,
+                             SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                             SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-  if (window == NULL) {
+  if (gWindow == NULL) {
     printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
-    exit(1);
+    return 1;
   }
 
-  SDL_Renderer *ren = SDL_CreateRenderer(
-      window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-  if (ren == NULL) {
+  gRenderer = SDL_CreateRenderer(
+      gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (gRenderer == NULL) {
     printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
-    exit(1);
+    return 1;
   }
+  return 0;
+}
 
-  SDL_Surface *screen = SDL_LoadBMP("LAND3.BMP");
-  if (screen == NULL) {
+int loadBMP() {
+  gScreen = SDL_LoadBMP("BLK.BMP");
+  if (gScreen == NULL) {
     printf("SDL_LoadBMP Error: %s\n", SDL_GetError());
+    return 1;
+  }
+  return 0;
+}
+
+int main(int argc, char *args[]) {
+  if (init()) {
     exit(1);
   }
 
-  SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, screen);
-  SDL_FreeSurface(screen);
+  if (loadBMP()) {
+    exit(1);
+  }
+
+  SDL_Texture *tex = SDL_CreateTextureFromSurface(gRenderer, gScreen);
+  SDL_FreeSurface(gScreen);
   if (tex == NULL) {
     printf("SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
     exit(1);
   }
 
-  SDL_Event e;
-  while (1) {
+  int close = 0;
+
+  while (!close) {
+    SDL_Event e;
     while (SDL_PollEvent(&e)) {
       switch (e.type) {
-        case SDL_KEYDOWN:
-          printf("keydown\n");
-          break;
         case SDL_QUIT:
+          close = 1;
           break;
-        case SDL_MOUSEBUTTONDOWN:
-          printf("i do know what that is\n");
-          break;
+        case SDL_KEYDOWN:
+          switch (e.key.keysym.scancode) {
+            case SDL_SCANCODE_W:
+            case SDL_SCANCODE_UP:
+              printf("up");
+              break;
+            case SDL_SCANCODE_A:
+            case SDL_SCANCODE_LEFT:
+              printf("left");
+              break;
+            case SDL_SCANCODE_S:
+            case SDL_SCANCODE_DOWN:
+              printf("down");
+              break;
+            case SDL_SCANCODE_D:
+            case SDL_SCANCODE_RIGHT:
+              printf("right");
+              break;
+          }
         default:
-          printf("hmm\n");
+          break;
       }
     }
-    SDL_RenderClear(ren);
-    SDL_RenderCopy(ren, tex, NULL, NULL);
-    SDL_RenderPresent(ren);
+    SDL_RenderClear(gRenderer);
+    SDL_RenderCopy(gRenderer, tex, NULL, NULL);
+    SDL_RenderPresent(gRenderer);
+    SDL_Delay(1000 / 60);
   }
 
   SDL_DestroyTexture(tex);
-  SDL_DestroyRenderer(ren);
-  SDL_DestroyWindow(window);
+  SDL_DestroyRenderer(gRenderer);
+  SDL_DestroyWindow(gWindow);
   SDL_Quit();
   return 0;
 }
